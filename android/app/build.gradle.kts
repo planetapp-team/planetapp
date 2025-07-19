9a7efb8a151b60bd9a80ac28262a4896308b6a7e
parkjoohee <jooheepark4816@gamil.com>
2025-07-19 19:53:54 +0900

    Fix: android/app/build.gradle.kts ndkVersion 27.0.12077973/minSdk 23 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 android {
     namespace = "com.example.planetapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -26,7 +26,7 @@ android {
         applicationId = "com.example.planetapp" // ✅ Firebase에 등록한 패키지명과 같아야 함
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
